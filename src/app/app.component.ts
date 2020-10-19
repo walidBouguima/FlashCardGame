01577c349eafa2663b52042918e75c83016f75b0
Walid Bouguima <walid.bouguima@gmail.com>
2020-10-19 12:32:19 +0100

    Implement the add functionality and wire things up in the form after activating validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild } from '@angular/core'
+import { NgForm } from '@angular/forms'
 
 import { IFlash } from './flash.model'
 
+// tslint:disable-next-line: typedef
 function getRandomNumber() {
   return Math.floor(Math.random() * 1000)
 }
@@ -12,9 +14,13 @@ function getRandomNumber() {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('flashForm', { static: true }) flashForm: NgForm
   editing = false
   editingId: number
-
+  flash = {
+    question: '',
+    answer: '',
+  }
   flashes: IFlash[] = [
     {
       question: 'Question 1',
@@ -58,12 +64,31 @@ export class AppComponent {
     const index = this.flashes.findIndex((flash) => Object.is(flash.id, id))
     this.flashes.splice(index, 1)
   }
+  // tslint:disable-next-line: typedef
   handleEdit(id: number) {
     this.editing = true
     this.editingId = id
   }
+  // tslint:disable-next-line: typedef
   handleRememberedChange({ id, flag }) {
+    // tslint:disable-next-line: no-shadowed-variable
     const flash = this.flashes.find((flash) => Object.is(flash.id, id))
     flash.remembered = flag
+  }
+  handleSubmit(): void {
+    this.flashes.push({
+      id: getRandomNumber(),
+      show: false,
+      ...this.flash,
+    })
+    this.handleClear()
+  }
+  // tslint:disable-next-line: typedef
+  handleClear() {
+    this.flash = {
+      question: '',
+      answer: '',
+    }
+    this.flashForm.reset()
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 import { FlashComponent } from './flash/flash.component'
 
 @NgModule({
   declarations: [AppComponent, FlashComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
