71bcdd92ed8b2af2d571c2e26c36c5cec8175328
Walid Bouguima <walid.bouguima@gmail.com>
2020-10-19 10:05:43 +0100

    Create a new component named flash that will hold all the logic and template of the card, create a data model in the root level, bind to the flash property to the data mdoel and initilaize it with dummy data in the flash component and bind that to the template to show the data fields

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { FlashComponent } from './flash/flash.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FlashComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/flash/flash.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core'
+
+import { IFlash } from '../flash.model'
+
+@Component({
+  selector: 'app-flash',
+  templateUrl: './flash.component.html',
+  styleUrls: ['./flash.component.css'],
+})
+export class FlashComponent implements OnInit {
+  @Input() flash: IFlash = {
+    id: 1,
+    question: 'React to Angular',
+    answer: 'No Reaction :)',
+    show: false,
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
+}
