ae799ffdd80dfe6e55229abeb171f3448db13995
Walid Bouguima <walid.bouguima@gmail.com>
2020-10-19 10:20:06 +0100

    Add array of flash objects and track them by id using randomly generated ids. Use trackBy built-in function to track changes in the DOM nodes via the generated ids

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core'
 
+import { IFlash } from './flash.model'
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 1000)
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-flashcards'
+  flashes: IFlash[] = [
+    {
+      question: 'Question 1',
+      answer: 'Answer 1',
+      show: false,
+      id: getRandomNumber(),
+    },
+    {
+      question: 'Question 2',
+      answer: 'Answer 2',
+      show: false,
+      id: getRandomNumber(),
+    },
+    {
+      question: 'Question 3',
+      answer: 'Answer 4',
+      show: false,
+      id: getRandomNumber(),
+    },
+    {
+      question: 'Question 4',
+      answer: 'Answer 4',
+      show: false,
+      id: getRandomNumber(),
+    },
+  ]
+
+  trackFlashById(flash) {
+    return flash.id
+  }
 }
